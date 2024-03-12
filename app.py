afee5b3670a089856cfd7319294ca5c94047f690
prasanga101 <126433443+prasanga101@users.noreply.github.com>
2024-03-12 23:10:37 +0545

    third commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, flash, redirect
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
+from pymongo import MongoClient
 import os
 
 app = Flask(__name__)
@@ -8,6 +9,36 @@ app = Flask(__name__)
 # Load the saved model
 model = load_model("my_models.h5")
 
+# Define MongoDB connection URI
+MONGODB_URI = "mongodb+srv://prasanga:cit340@cluster0.vioooj7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Create a MongoClient object
+client = MongoClient(MONGODB_URI)
+
+# Select the database and collection
+db = client['Prasanga']
+collection = db['data']
+def download_image(file_id, destination_folder):
+    # Query the database to retrieve the document containing the image data
+    document = collection.find_one({"_id": bson.ObjectId(file_id)})
+
+    if document:
+        # Extract the image data
+        image_data = document['image_data']
+
+        # Specify the destination file path
+        destination_file_path = os.path.join(destination_folder, document['filename'])
+
+        # Save the image data to a file
+        with open(destination_file_path, 'wb') as file:
+            file.write(image_data)
+
+        print("Image downloaded successfully:", destination_file_path)
+    else:
+        print("Document not found.")
+
+# Usage example
+download_image("<file_id>", "/uploads")
 # Define a function to preprocess the image before feeding it to the model
 def preprocess_image(image_path):
     img = image.load_img(image_path, target_size=(32, 32))
@@ -48,7 +79,10 @@ def index():
                 result = "Male"
             else:
                 result = "Female"
+            # Insert the filename and prediction result into MongoDB
+            db_entry = {"filename": filename, "prediction": result}
+            collection.insert_one(db_entry)
     return render_template('index.html', result=result)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
